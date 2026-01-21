2835f71f78e8ae3971beba79b75021d9ded84800
WangleyVieira <wangleymsv10@hotmail.com>
2026-01-20 21:43:10 -0400

    feat: definir políticas de segurança e acesso com Spring Security (#6)

---
+++ src/main/java/com/wangley/musicapi/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.wangley.musicapi.security;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                // 3️⃣ Evita redirect para login (retorna 401)
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(
+                                (request, response, authException) ->
+                                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED)
+                        )
+                );
+
+        return http.build();
+    }
+}
